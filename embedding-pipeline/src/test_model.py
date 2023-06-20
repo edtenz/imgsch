d565724954b6954335fb7e31bd895c1cfa6b02f1
Eddie Tenz <tzz1002@gmail.com>
2023-06-20 23:08:27 +0800

    test

--- embedding-pipeline/src/test_model.py
+++ embedding-pipeline/src/test_model.py
@@ -35,7 +35,6 @@ def create_milvus_collection(collection_name: str = DEFAULT_TABLE, dim: int = 19
     fields = [
         FieldSchema(name='key', dtype=DataType.VARCHAR, description='image key', max_length=40,
                     is_primary=True, auto_id=False),
-        FieldSchema(name='sbox', dtype=DataType.VARCHAR, description='image box', max_length=20),
         FieldSchema(name='vec', dtype=DataType.FLOAT_VECTOR, description='image embedding vectors', dim=dim)
     ]
     schema = CollectionSchema(fields=fields, description='reverse image search')
@@ -43,7 +42,7 @@ def create_milvus_collection(collection_name: str = DEFAULT_TABLE, dim: int = 19
 
     index_params = {
         'metric_type': 'L2',
-        'index_type': 'IVF_SQ8',
+        'index_type': 'IVF_FLAT',
         'params': {"nlist": dim}
     }
     collection.create_index(field_name='vec', index_params=index_params)
@@ -59,7 +58,7 @@ def test_vit224_extract_pipeline():
     model = Vit224()
     p_insert = (
         model.pipeline()
-        .map(('key', 'sbox', 'vec'), 'mr', ops.ann_insert.milvus_client(
+        .map(('key', 'vec'), 'mr', ops.ann_insert.milvus_client(
             host=MILVUS_HOST,
             port=MILVUS_PORT,
             collection_name=DEFAULT_TABLE,
@@ -82,11 +81,11 @@ def test_vit224_extract_pipeline():
             host=MILVUS_HOST, port=MILVUS_PORT, limit=10,
             collection_name=DEFAULT_TABLE))
         .map('search_res', ('pred', 'distance'), lambda x: ([y[0] for y in x], [y[1] for y in x]))
-        .output('pred', 'distance')
+        .output('pred', 'distance', 'search_res')
     )
     collection.load()
-    res = p_search_pre('../data/fruit.png')
+    res = p_search_pre('../data/objects.png')
     print(f'Number of search results: {res.size}')
     for i in range(res.size):
         it = res.get()
-        print(f'{i}, pred: {it[0]}, distance: {it[1]}')
+        print(f'{i}, search_res: {it[2]}')
