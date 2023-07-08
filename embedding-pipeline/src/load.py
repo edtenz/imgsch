288d511dbbc5c28786425e19dcbea2c513c8c808
Eddie Tenz <tzz1002@gmail.com>
2023-07-08 11:14:08 +0800

    merge load

--- embedding-pipeline/src/load.py
+++ embedding-pipeline/src/load.py
@@ -1,11 +1,14 @@
 import json
 
 import requests
+from towhee import pipe
 
-from config import DEFAULT_TABLE, VECTOR_DIMENSION, MINIO_PROXY_ENDPOINT
+from config import DEFAULT_TABLE, VECTOR_DIMENSION
+from config import ES_INDEX, MINIO_PROXY_ENDPOINT
+from es_helpers import EsClient, insert_img_doc_ops, create_img_index
 from logger import LOGGER
 from milvus_helpers import MilvusClient, insert_milvus_ops
-from model import ImageFeatureModel
+from model import ImageFeatureModel, extract_features_ops
 from mysql_helpers import MysqlClient, insert_mysql_ops
 
 
@@ -76,4 +79,67 @@ def embedding_milvus_pipe(img_url: str,
             f'id: {it[4]}, db_res: {it[5]}')
 
     LOGGER.debug(f"Process file {img_url} successfully")
+    return True
+
+
+def do_es_embedding(
+        bucket_name: str,
+        model: ImageFeatureModel,
+        es_cli: EsClient,
+        index_name: str = ES_INDEX,
+        max_count: int = 0) -> int:
+    create_img_index(es_cli, index_name)
+
+    lst_url = f'http://{MINIO_PROXY_ENDPOINT}/file/{bucket_name}'
+    LOGGER.info(f"List url: {lst_url}")
+    response = requests.get(lst_url)
+    LOGGER.debug(f"Response: {response}")
+    response.raise_for_status()  # Raise an exception if the request was unsuccessful
+
+    object_names = json.loads(response.content)
+    if 0 < max_count < len(object_names):
+        object_names = object_names[:max_count]
+
+    total = len(object_names)
+    LOGGER.info(f"Start to process {total} files")
+    success_count = 0
+    for i, object_name in enumerate(object_names):
+        LOGGER.info(f"Process file {object_name}, {i + 1}/{total}")
+        img_url = f'http://{MINIO_PROXY_ENDPOINT}/file/{bucket_name}/{object_name}'
+        try:
+            ok = embedding_es_pipe(img_url, model, es_cli, index_name)
+            if ok:
+                success_count += 1
+
+            LOGGER.info(f"Process file {object_name} successfully, succ count: {success_count}/{total}")
+        except Exception as e:
+            LOGGER.error(f"Process file {object_name} failed: {e}")
+            continue
+
+    return success_count
+
+
+def embedding_es_pipe(img_url: str,
+                      model: ImageFeatureModel,
+                      es_cli: EsClient,
+                      index_name: str = ES_INDEX) -> bool:
+    p_insert = (
+        pipe.input('url')
+        .map('url', 'key', lambda x: x.split('/')[-1].split('.')[0])
+        .filter(('key',), ('key',), 'key', lambda x: x is not None and len(x) > 0)
+        .map('url', ('sbox', 'label', 'score', 'features'), extract_features_ops(model))
+        .filter(('features',), ('features',), 'features', lambda x: x is not None and len(x) > 0)
+        .map('features', 'fsize', lambda x: len(x))
+        .map(('key', 'url', 'sbox', 'score', 'label', 'features', 'key'), 'res', insert_img_doc_ops(es_cli, index_name))
+        .output('key', 'url', 'sbox', 'score', 'label', 'fsize', 'key', 'res')
+    )
+
+    res = p_insert(img_url)
+    size = res.size
+    if size == 0:
+        LOGGER.info("no result")
+        return False
+    for i in range(size):
+        it = res.get()
+        LOGGER.info(f'inserted: {it}')
     return True
--- embedding-pipeline/src/test_load.py
+++ embedding-pipeline/src/test_load.py
@@ -1,13 +1,39 @@
 from towhee import pipe
 
+from config import ES_HOST, ES_PORT, ES_INDEX, MINIO_BUCKET_NAME
+from es_helpers import EsClient, create_img_index
 from load import do_milvus_embedding
+from load import embedding_es_pipe, do_es_embedding
 from milvus_helpers import MilvusClient
 from model import VitBase224
 from model import extract_features_ops
 from mysql_helpers import MysqlClient
 
 
-def test_process():
+def test_embedding_es_pipe():
+    es_cli = EsClient(host=ES_HOST, port=ES_PORT)
+    index_name = ES_INDEX
+    ok = create_img_index(es_cli, index_name)
+    print("ok: ", ok)
+    if not ok:
+        return
+
+    img_url = 'http://localhost:10086/file/imgsch/0ad09b3fdb4fbd661923fb5555c7b341.jpg'
+    model = VitBase224()
+    res = embedding_es_pipe(img_url, model, es_cli, index_name)
+    print("res: ", res)
+
+
+def test_do_es_embedding():
+    es_cli = EsClient(host=ES_HOST, port=ES_PORT)
+    index_name = ES_INDEX
+    model = VitBase224()
+    test_count = 10000
+    suc = do_es_embedding(MINIO_BUCKET_NAME, model, es_cli, index_name, test_count)
+    print("suc: ", suc)
+
+
+def test_do_milvus_embedding():
     vit_model = VitBase224()
     mysql_cli = MysqlClient()
     milvus_cli = MilvusClient()
