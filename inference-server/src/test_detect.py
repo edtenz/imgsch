525aa3e71c21a71e4066f7e7fbe916542916d336
Eddie Tenz <tzz1002@gmail.com>
2023-07-12 07:35:55 +0800

    remove infer service

--- inference-server/src/test_detect.py
+++
@@ -1,9 +0,0 @@
-from detect import DETECTOR
-
-
-def test_detect_file():
-    img = '../data/objects.png'
-    res = DETECTOR.detect(img)
-    for item in res:
-        print(item)
-    assert len(res) == 2
